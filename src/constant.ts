7f38a18fa73689fa25c959a5a1a5ea1939b4a4c6
backin6643 <heyingting@gmail.com>
2025-12-29 13:10:14 +0800

    Update constant.ts

--- src/constant.ts
+++ src/constant.ts
@@ -10,14 +10,14 @@ export const AESKEY_DEFAULT = 'LSKDAJALSD';
  * GARMIN ACCOUNT
  */
 // 佳明中国区账号及密码
-export const GARMIN_USERNAME_DEFAULT = '';
-export const GARMIN_PASSWORD_DEFAULT = '';
+export const GARMIN_USERNAME_DEFAULT = 'heyt1983@163.com';
+export const GARMIN_PASSWORD_DEFAULT = 'Heyt1012';
 // 佳明国际区区账号及密码
-export const GARMIN_GLOBAL_USERNAME_DEFAULT = '';
-export const GARMIN_GLOBAL_PASSWORD_DEFAULT = '';
+export const GARMIN_GLOBAL_USERNAME_DEFAULT = 'backin6643@gmail.com';
+export const GARMIN_GLOBAL_PASSWORD_DEFAULT = 'Q1w2e3_$';
 // 佳明迁移数量配置
-export const GARMIN_MIGRATE_NUM_DEFAULT = '';
-export const GARMIN_MIGRATE_START_DEFAULT = '';
+export const GARMIN_MIGRATE_NUM_DEFAULT = '10';
+export const GARMIN_MIGRATE_START_DEFAULT = '0';
 // 佳明每次同步时检查的最多的数量
 export const GARMIN_SYNC_NUM_DEFAULT = 10;
 
